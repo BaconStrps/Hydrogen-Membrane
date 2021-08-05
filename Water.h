f7e5709b5f942f305ae94ae1250f10833d9ace88
baconstrps <jaredcochran@outlook.com>
2021-08-05 15:46:32 -0400

    Split class headers into separate implementation files

--- Water.h
+++ Water.h
@@ -1,5 +1,4 @@
 #define WATER 1
-#include <iostream>
 
 class Water
 {
@@ -12,30 +11,6 @@ class Water
 		double totalcount[4] = {0, 0, 0, 0};
 		char name[3];
 		
-		void setH1(float x, float y, float z, char n)
-		{
-			name[1] = n;
-			h1[0] = x;
-			h1[1] = y;
-			h1[2] = z;
-		}
-		
-		void setH2(float x, float y, float z, char n)
-		{
-			name[2] = n;
-			h2[0] = x;
-			h2[1] = y;
-			h2[2] = z;
-		}
-		
-		void setO(float x, float y, float z, char n)
-		{
-			name[3] = n;
-			o[0] = x;
-			o[1] = y;
-			o[2] = z;
-		}
-		
 		/*
 			If above,
 				Oxygen z coord > hydrogen z coords
@@ -45,53 +20,10 @@ class Water
 				Memb z coord - oxygen z coord < dist & > 0
 		*/
 		
-		int checkifnear(double accdisp, double membz, bool ab)
-		{
-			if (ab && (o[2] > h1[2] && o[2] > h2[2]) && (o[2] - membz < accdisp && o[2] - membz > 0))
-				return 1;
-			else if (ab && (o[2] - membz < accdisp && o[2] - membz > 0))
-				return 2;
-			else if (!ab && (o[2] < h1[2] && o[2] < h2[2]) && (membz - o[2] < accdisp && membz - o[2] > 0))
-				return 1;
-			else if (!ab && (membz - o[2] < accdisp && membz - o[2] > 0))
-				return 2;
-			else
-				return false;
-				
-		}
+		int checkifnear(double accdisp, double membz, bool ab);
 		
-		void checkResult(double memb[4], double accdisp)
-		{
-				if (checkifnear(accdisp, memb[0], false) == 1) // below, outside
-				{
-					goodcount[0]++;
-					totalcount[0]++;
-				}
-				else if (checkifnear(accdisp, memb[0], false) == 2)
-					totalcount[0]++;
-				else if (checkifnear(accdisp, memb[1], true) == 1) // above, inside
-				{
-					goodcount[1]++;
-					totalcount[1]++;
-				}
-				else if (checkifnear(accdisp, memb[1], true) == 2)
-					totalcount[1]++;	
-			
-				if (checkifnear(accdisp, memb[2], false) == 1) // below, inside
-				{
-					goodcount[2]++;
-					totalcount[2]++;
-				}
-				else if (checkifnear(accdisp, memb[2], false) == 2)
-					totalcount[2]++;
-				else if (checkifnear(accdisp, memb[3], true) == 1) // above, outside
-				{
-					goodcount[3]++;
-					totalcount[3]++;
-				}
-				else if (checkifnear(accdisp, memb[3], true) == 2)
-					totalcount[3]++;
-		}
+		void checkResult(double memb[4], double accdisp);
+
 		
 		
 };
